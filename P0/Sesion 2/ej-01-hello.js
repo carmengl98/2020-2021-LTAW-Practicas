4804c4c7c2c27637ff1f91ced9030777f16d2b5e
carmengl98 <c.gonzalezlo.2016@alumnos.urjc.es>
2021-02-22 19:05:46 +0100

    ejemplos para recordar js

---
+++ P0/Sesion 2/ej-01-hello.js
@@ -0,0 +1,8 @@
+//Hola mundo en Node.js
+
+/*Esto es un comentario multilinea*/
+/*El objeto console está disponible directamente
+   desde node.js, sin tener que importar nada */
+
+//-- Imprimir un mensaje en la consola
+console.log("¡Hola Mundo!");
---
+++ P0/Sesion 2/ej-03-bucles.js
@@ -0,0 +1,12 @@
+//-- Ejemplo de bucles
+
+//-- Definiendo una constante: Número de mensajes
+/*const = constante*/
+/*No puedes asignar un valor a una contante tienes que declarar una variable*/
+/*A una misma variable se le puede cambiar el valor*/
+const N = 10; 
+
+//-- Bucle para imprimir N mensajes
+for (i = 0; i < N; i++) {
+    console.log("Mensaje " + i);
+}
