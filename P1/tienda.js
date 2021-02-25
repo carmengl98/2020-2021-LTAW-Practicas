743c40bd079cc0939da237840fa5537fe409b30e
carmengl98 <c.gonzalezlo.2016@alumnos.urjc.es>
2021-02-25 16:06:44 +0100

    Cambios de prueba

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 //-- Definir el puerto a utilizar
-const PUERTO = 8000;
+const PUERTO = 9000;
 
 //-- Crear el servidor
 const server = http.createServer((req, res) => {
