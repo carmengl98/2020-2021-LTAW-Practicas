be4558683b6bdda2e6cd1e37174bd0ff6959bdc8
carmengl98 <c.gonzalezlo.2016@alumnos.urjc.es>
2021-04-04 11:29:34 +0200

    arreglos servidor

--- P1/server-tienda.js
+++ P1/server-tienda.js
@@ -8,69 +8,67 @@ const PUERTO = 9000;
 
 //-- Crear el servidor
 const server = http.createServer((req, res) => {
-
-  const objetourl = url.parse(req.url);
-  let camino=''+objetourl.pathname;
-  console.log('Holaaaaaaaaaa',camino);
-
-  //-- Indicamos que se ha recibido una petición
-  console.log("Petición recibida!!");
-  console.log(req.url); 
   
   const myURL = new URL(req.url, 'http://' + req.headers['host']);
-  console.log("Recurso recibido: " + myURL.pathname);
+  //console.log("Recurso recibido: " + myURL.pathname);
 
-  var mime2 = {
+  //-- Indicamos que se ha recibido una petición
+  console.log("----------> Petición recibida:",myURL);
+
+  var mime = {
+    '/' : 'text/html',
     'html' : 'text/html',
     'css'  : 'text/css',
     'jfif'  : 'image/jfif',
- };
-  // -- Buscamos el "." final para poder indicar que tipo mime es
-  let hastaPunto = myURL.pathname.lastIndexOf(".");
-  let type = myURL.pathname.slice(hastaPunto+1);
-  let typeMime = type;
-  console.log("mime",mime2[type]);
+    'gif'  : 'image/gif',
+  
+  };
 
-  /* var cadenaDeTexto = "https://www.midominio.com/sites/document/indice1.txt";
-  var hastaSites = cadenaDeTexto.indexOf('/document');
-  console.log(hastaSites);
-  console.log(cadenaDeTexto.substring(0, hastaSites+1));
- */
+  let filename = ""
+  
+  //-- Obtenemos el fichero correspondiente.
+  if(myURL.pathname == '/'){
+    filename += "./tienda.html"; //-- Página principal de la tienda
+    // -- Buscamos el "." final para poder indicar que tipo mime es
+    let hastaPunto = myURL.pathname.lastIndexOf(".");
+    let type = myURL.pathname.slice(hastaPunto+1);
+    console.log("Tipo de mime:",mime[type]);
+  }else{
+    filename = "." + myURL.pathname;
+    // -- Buscamos el "." final para poder indicar que tipo mime es
+    let hastaPunto = myURL.pathname.lastIndexOf(".");
+    let type = myURL.pathname.slice(hastaPunto+1);
+    console.log("Tipo de mime:",mime[type]);
+  
+  }
+   
+  console.log("Filename:",filename);
 
   //-- Valores de la respuesta por defecto
   let code = 200;
   let message = "OK";
-  //-- Fichero a leer
-  let page = "tienda.html";
 
-  //-- Cualquier recurso que no sea la página principal
-  //-- genera un error
-   if (myURL.pathname != '/') {
+  //-- Leer fichero
+  fs.readFile(filename, function(err, data) {
+     //-- Fichero no encontrado. Devolver mensaje de error
+    if (err) {
       code = 404;
-      message  = "Not Found";
-      //-- Fichero a leer
-      page = "tienda_error.html";
-   }
-
-
-  //-- Definir tipo de archivo html.
-  var mime = "text/html"
-
-  const data = fs.readFileSync(page,'utf8');
+      message = "Not Found";
+      return res.end("404 Not Found");
+    }
+    //console.log("Respuesta: 200 OK")
+      
+    res.statusCode = code; 
+    res.statusMessage = message;
+    res.writeHead(code, {'Content-Type': mime});
+    res.write(data);
+    res.end();
     
-  // const data = fs.readFileSync(FICHERO, 'utf8');
-  console.log("Lectura completada...")
-  console.log("Contenido del fichero: \n")
+  });
+  
+  //console.log("Contenido del fichero: \n")
   //console.log(data); 
-  
-
-  res.statusCode = code; // VEEER MEJOR
-  res.statusMessage = message;
-  res.writeHead(code, {'Content-Type': mime});
-  res.write(data);
-  res.end();
-
-  
+   
 });
 
 
