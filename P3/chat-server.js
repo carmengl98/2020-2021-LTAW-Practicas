5935d463cc31152196373f038b7526da6b8b6a12
carmengl98 <c.gonzalezlo.2016@alumnos.urjc.es>
2021-05-28 11:09:38 +0200

    cambios

--- P3/chat-server.js
+++ P3/chat-server.js
@@ -6,14 +6,8 @@ const colors = require('colors');
 
 const PUERTO = 9000;
 
+var user = 0;
 
-const message_listcommand = 'help: Mostrará una lista con todos los comandos soportados' +
-            '/list: Devolverá el número de usuarios conectados' +
-'/hello: El servidor nos devolverá el saludo' +
-'/date: Nos devolverá la fecha';
-const message_hello = '¡¡Bienvenido al chat!!';
-const message_data = 'Fecha:';
-const message_users = 'users';
 
 
 //-- Crear una nueva aplciacion web
@@ -43,19 +37,55 @@ app.use(express.static('public_chat'));
 io.on('connect', (socket) => {
   
   console.log('** NUEVA CONEXIÓN **'.yellow);
+  
+  user = user + 1;
+  console.log('Usuarios conectados:'.green, user);
 
   //-- Evento de desconexión
   socket.on('disconnect', function(){
     console.log('** CONEXIÓN TERMINADA **'.yellow);
+    if (user >= 0){
+      console.log('Usuarios conectados:'.green, user);
+    }
   });  
 
   //-- Mensaje recibido: Reenviarlo a todos los clientes conectados
   socket.on("message", (msg)=> {
     console.log("Mensaje Recibido!: " + msg.blue);
+    switch(msg){
+      case '/help':
+        const message_listcommand = '/help --> Mostrará una lista con todos los comandos soportados.' + '<br>' +
+                            '/list --> Devolverá el número de usuarios conectados.' + '<br>' +
+                            '/hello --> El servidor nos devolverá el saludo.' + '<br>' +
+                            '/date --> Nos devolverá la fecha.';
+
+        io.send(message_listcommand);
+        break;
+      case '/hello':
+        const message_hello = '¡¡Bienvenido al chat!!';
+        io.send(message_hello);
+        break;
+      case '/date':
+        d = new Date();
+        const message_date = 'Fecha: ' + d.getDate() +'/'+ d.getMonth() +'/' + d.getFullYear();
+        io.send(message_date);
+        break;
+      case '/list': 
+        const message_users = 'Usuarios conectados: ' + user;
+        io.send(message_users);
+      break;
+      default:
+        if(msg[0] == '/'){
+          io.send('El comando es erroneo!!')
+        }else{
+          socket.send(msg);
+        }
+        return;
+    }
 
     //-- Reenviarlo a todos los clientes conectados
-    io.send(msg);
-    socket.send(msg);
+    //io.send(msg);
+    //socket.send(msg);
     //io.send(data);--> envia  a todos los clientes que están actualmente conectados
 
   });
